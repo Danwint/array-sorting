d9b1de9a7b16a43684e621befbd62c728fbca2f2
David Wintrich <david@techelevator.com>
2015-09-12 17:57:59 -0400

    modified array initialization to use randomly generated numbers

--- ArraySort.java
+++ ArraySort.java
@@ -1,13 +1,25 @@
+import java.util.Random;
+
 public class ArraySort {
 	public static void main(String[] args) {
-		int[] numbers = new int[] {3, 7, 2, 4, 1, 8, 9, 5, 6, 10};
+		int[] numbers = new int[10];  // This is the array to sort
+		Random rand = new Random();
+		System.out.print("Before sorting: {");
+		for(int i = 0; i < numbers.length; i++) {
+			numbers[i] = rand.nextInt(100)+1;
+			System.out.print(numbers[i]);
+			if(i < numbers.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("}");
 		// ∧ ∧ ∧ ∧ ∧ ∧ ∧ LEAVE THE CODE ABOVE THIS LINE ALONE!!! ∧ ∧ ∧ ∧ ∧ ∧ ∧ 
 		
-		// YOUR CODE GOES HERE
+		// YOUR CODE TO SORT THE numbers ARRAY GOES HERE
 		
 		// ∨ ∨ ∨ ∨ ∨ ∨ ∨ LEAVE THE CODE BELOW THIS LINE ALONE!!! ∨ ∨ ∨ ∨ ∨ ∨ ∨  
 		boolean isWrong = false;
-		System.out.print("{");
+		System.out.print("After sorting: {");
 		for(int i = 0; i < numbers.length; i++) {
 			System.out.print(numbers[i]);
 			if(i < numbers.length - 1) {
